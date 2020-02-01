5e59647d6a694851d45673177e2a85f670f71100
unknown <casper@caspers.club>
2020-01-31 17:15:08 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* jshint esversion: 8 */
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+const images = require('./routes/images');
+
+app.use(fileUpload());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use('/', images);
+
+app.listen(80, () => console.log(`listening on port ${80}, amirite`));
+
+
---
+++ routes/images.js
@@ -0,0 +1,40 @@
+/*jshint esversion: 8 */
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+router.get(`/:name`, async (req, res) => {
+    try {
+        if(!req.params.name) return res.status(500).send("yeah so uhm, this is an api, you know an api?");;
+        if (!fs.existsSync(__dirname + `/../images/${req.params.name}.png`)) {
+            return res.status(400).send("Cannot find image");
+        }
+
+        if (req.query) return res.status(200).sendFile(path.resolve(__dirname + `/../images/${req.params.name}.png`));
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("this shit broken bruh");
+    }
+});
+
+router.post("/save/:name", async (req, res) => {
+    try {
+        if(!req.header("token") == "ufocoolaf") return res.status(401).send("you arent ufo, you cunt");
+        if(!req.body) return res.status(200).send("yeah no bruh, you didnt provide an image");
+        if (fs.existsSync(__dirname + `/../images/${req.params.name}`)) {
+            return res.status(500).send("Bruh, that image already exists");
+        }
+
+        //console.log()
+        
+        req.files['files[]'].mv(__dirname + `/../images/${req.params.name}`)
+        
+        if (req.query) return res.status(200).json({name: req.params.name.replace(".png", "")});
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("this shit broken bruh");
+    }
+});
+
+module.exports = router;
